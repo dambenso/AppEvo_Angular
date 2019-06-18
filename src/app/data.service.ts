c950e9cc7c2a5501bb488ff3f17c90da90828c26
Damon Benson <dambenso@progress.com>
2019-06-18 14:18:36 -0400

    Latest changes
    Update of latest changes

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -7,12 +7,16 @@ import { HttpClient } from "@angular/common/http";
 export class ProgressRESTService {
   constructor(private httpClient: HttpClient) {}
 
-  system_REST_URL = "http://localhost:16161/POS/web/customerservice/";
+  customer_REST_URL = "http://nbnordambenso:18181/POS/web/customerservice/";
+  item_REST_URL = "http://nbnordambenso:18181/POS/web/pdo/itemSVC/ItemEntity";
 
   getCustomerData(custnum) {
-    return this.httpClient.get(this.system_REST_URL + "customer/" + custnum);
+    return this.httpClient.get(this.customer_REST_URL + "customer/" + custnum);
   }
   getCustomersData() {
-    return this.httpClient.get(this.system_REST_URL + "getCustomers/");
+    return this.httpClient.get(this.customer_REST_URL + "getCustomers/");
+  }
+  allItemData_sansImage() {
+    return this.httpClient.get(this.item_REST_URL);
   }
 }
--- src/app/ordercatsel/ordercatsel.component.ts
+++ src/app/ordercatsel/ordercatsel.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { Router, NavigationExtras, ActivatedRoute } from "@angular/router";
+import { ProgressRESTService } from "../data.service";
+import { groupBy, GroupResult } from "@progress/kendo-data-query";
 
 @Component({
   selector: "ordercatsel",
@@ -6,6 +9,28 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./ordercatsel.component.scss"]
 })
 export class OrderCatSelComponent implements OnInit {
-  ngOnInit() {}
-  onButtonClick() {}
+  ITM$: Object;
+  public ITD$: Object;
+  errorMessage: string;
+  public pcategories: any[] | GroupResult[];
+  public scategories: any[] | GroupResult[];
+  public convd: any[] = [];
+
+  constructor(private data: ProgressRESTService, private router: Router) {}
+  ngOnInit() {
+    this.data.allItemData_sansImage().subscribe(
+      data => {
+        this.ITM$ = data;
+        this.ITD$ = [];
+        this.ITD$ = this.ITM$["dsItem"].ttItem;
+        this.convd = JSON.parse(JSON.stringify(this.ITD$));
+        this.pcategories = groupBy(this.convd, [
+          { field: "Category2" },
+          { field: "Category1" }
+        ]);
+      },
+      error => (this.errorMessage = <any>error)
+    );
+  }
 }
+function acceptClick(ctype) {}
